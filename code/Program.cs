f0519e121b4be08655f2c4d47ed047b4a59d7179
natasha <gromc-natalija@rambler.ru>
2022-11-02 01:25:37 +0300

    написала код программы

---
+++ code/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int N;
+Console.Write($"Введите количество элементов массива: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+String[] array = new String[N];
+
+for (int curind = 0; curind < N; curind++)
+{
+    Console.Write($"Введите элемент номер {curind} ");
+    array[curind] = Console.ReadLine()!;
+} 
+
+
+String[] array_new = new String[N];
+int i = 0;
+
+for (int curind=0;curind<array.Length;curind++)
+{
+    if (array[curind].Length <= 3)
+    {
+        array_new[i] = array[curind];
+    }
+    i++;
+}
+
+for (int j=0;j<i;j++)
+{
+    Console.Write($"{array_new[j]} ");
+}
